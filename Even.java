a6b5d274c3c73aac5d37a1b7b3c96c1d36d95cf6
ANULOGANATHAN <anusuya.lcse@ksrce.ac.in>
2016-07-21 14:27:13 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,19 @@
+# anu
+
+
+public class Even {
+
+	public static void main(String[] args) {
+	Scanner scan =new Scanner(System.in);
+	int s=scan.nextInt();
+	int n=s%2;
+	if(n==0){
+		System.out.println("even");
+	}
+	else{
+		System.out.println("odd");
+	}
+
+	}
+
+}
