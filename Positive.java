0aaf970483a6449008278b8757ec202500fc189e
ANULOGANATHAN <anusuya.lcse@ksrce.ac.in>
2016-07-21 14:18:13 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,14 @@
+# anu
+
+public class Positive {
+public static void main(String[] args) {
+	Scanner scan= new Scanner(System.in);
+	int a=scan.nextInt();
+	if(a>0){
+		System.out.println("given number is positive");
+	}
+	else {
+		System.out.println("given number is negative");
+	}
+}
+}
