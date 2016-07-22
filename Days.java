a980274ee317d1258c290b31d34f040a83941b9e
ANULOGANATHAN <anusuya.lcse@ksrce.ac.in>
2016-07-22 11:16:01 +0530

    Update and rename README.md to Days.java

---
+++ Days.java
@@ -0,0 +1,20 @@
+# anu
+
+
+public class Days {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		switch(str){
+		case "sunday":
+			System.out.println("false");
+			break;
+		default:System.out.println("true");
+		break;
+		}
+
+	}
+
+}
+
