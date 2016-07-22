8a96c472f1b7f4fa03e6b97053cdfb2b2f756947
ANULOGANATHAN <anusuya.lcse@ksrce.ac.in>
2016-07-22 15:16:43 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,15 @@
+# anu
+
+ublic class Sum {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int sum=0;
+		int n=scan.nextInt();
+	for(int i=0;i<=n;i++){
+	sum+=i;
+	}
+System.out.println(sum);
+	}
+
+}
