2eb9922417d8b8ef646e0ce031dbe3bdedac6acf
ANULOGANATHAN <anusuya.lcse@ksrce.ac.in>
2016-07-23 09:59:33 +0530

    Create WordReverse.java

---
+++ WordReverse.java
@@ -0,0 +1,14 @@
+# anu
+
+
+public class WordReverse {
+
+	public static void main(String[] args) {
+	String s="nice day";
+
+	StringBuffer buffer=new StringBuffer(s);
+	StringBuffer a=buffer.reverse();
+	System.out.println(a);
+	}
+
+}
